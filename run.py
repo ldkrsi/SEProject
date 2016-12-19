286b871dfea8b59eab0b939bb8c9f7be3b2070de
ldkrsi <louie810405@livemail.tw>
2016-12-20 01:16:02 +0800

    send_transaction_asyn

--- run.py
+++ run.py
@@ -14,5 +14,27 @@ def example(account_address):
 	i['paper'] = i['paper'].address
 	return str(p)+'<br>'+str(i)
 
+@app.route('/transactions')
+def transactions():
+	from model import rds,web3
+	from flask import render_template
+	import json, datetime
+
+	data_list = sorted(rds.keys(),reverse=True)
+	result = []
+	for key in data_list:
+		value = json.loads(rds.get(key).decode('utf-8'))
+		if value[0] is False:
+			ans = web3.eth.getTransactionReceipt(value[1])
+			value[0] = True if ans is not None else False
+			rds.set(key,json.dumps(value))
+		result.append([
+			datetime.datetime.fromtimestamp(
+				int(float(key.decode('utf-8')))
+			).strftime('%Y-%m-%dT%H:%M:%SZ'),
+			value
+		])
+	return render_template('transactions.html', result=result)
+
 if __name__ == "__main__":
 	app.run(host= '0.0.0.0')
